1ee07416b7d896195ca5cecb1b8008cd3903ba0c
edmond <edmondmulera55@gmail.com>
2024-04-02 09:31:09 +0300

    creating models

---
+++ web/portfolio/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.FileField(upload_to="projects", blank=True)
+    info = models.CharField(max_length=200)
